933969343dc62f030359e8df8a15300419f8b4c3
evan <evandromariano49@gmail.com>
2024-04-03 09:54:39 -0300

    feat: add sum and sub

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/pages/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: 'input', pathMatch: 'full' },
+
+  {
+    path: 'input',
+    component: MainPageComponent,
+    pathMatch: 'full',
+  },
+
+
+];
---
+++ src/app/components/pages/main-page/main-page.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { MenuBarComponent } from '../../shared/menu-bar/menu-bar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [MenuBarComponent, MatIconModule, FormsModule],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+  input: string = '4+5';
+  output: string = '';
+
+  sumSubOperation(expression: string) {
+    let regexExpressionPattern: RegExp = /([+-]?\d+)/g;
+    let parts: string[] = [];
+    let matchResult: RegExpExecArray | null;
+    let result: number = 0
+
+    while ((matchResult = regexExpressionPattern.exec(expression)) !== null) {
+      parts.push(matchResult[1]);
+    }
+
+    console.log(parts);
+    parts.forEach(expressionPart => {
+      result = result + Number(expressionPart);
+    });
+
+    console.log(result);
+    return result.toString();
+  }
+
+  result() {
+    this.output = this.sumSubOperation(this.input);
+  }
+}
