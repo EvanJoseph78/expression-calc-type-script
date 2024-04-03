e96bb351a35c01ab985b9da8ffbd29d023a7494c
evan <evandromariano49@gmail.com>
2024-04-03 13:15:21 -0300

    feat: float numbers

--- src/app/components/pages/main-page/main-page.component.ts
+++ src/app/components/pages/main-page/main-page.component.ts
@@ -11,11 +11,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-  input: string = '';
+  input: string = '(2+3)';
   output: string = '';
 
-  sumSubOperation(expression: string) {
-    let regexExpressionPattern: RegExp = /([+-]?\d+)/g;
+  /**
+   * Calcula a soma de partes em uma expressão matemática.
+   * @param expression A expressão matemática contendo números e operadores de adição/subtração.
+   * @returns Uma string representando o resultado da soma de todas as partes da expressão.
+   */
+
+  sumSubOperation(expression: string): string {
+    let regexExpressionPattern: RegExp = /([+-]?\d+.?\d*)/g;
     let parts: string[] = [];
     let matchResult: RegExpExecArray | null;
     let result: number = 0
@@ -24,20 +30,69 @@ export class MainPageComponent {
       parts.push(matchResult[1]);
     }
 
-    console.log(parts);
     parts.forEach(expressionPart => {
       result = result + Number(expressionPart);
     });
 
-    console.log(result);
     return result.toString();
   }
 
+  multOperation() {
+    let regexExpressionPattern: RegExp = /([+-]?\d+)/g;
+  }
+
+  parentesisExpression(expression: string): string {
+    let regexExpressionPattern: RegExp = /\(([^()]+)\)/g;
+
+    let matchResult = expression.match(regexExpressionPattern);
+
+    if (matchResult) {
+      let expressionPart = matchResult[0];
+      expressionPart = expressionPart.replace("(", "");
+      expressionPart = expressionPart.replace(")", "");
+      let result = this.mainFunction(expressionPart);
+      expression = expression.replace(matchResult[0], result);
+    }
+
+    return expression;
+  }
+
+  mainFunction(expression: string): string {
+
+    if (this.containsInvalidCharacters(expression)) {
+      return "Expressão inválida!"
+    }
+
+    if (expression.includes("(")) {
+      expression = this.parentesisExpression(expression);
+    }
+
+    if (expression.includes("*")) {
+
+      return "multiplicação em breve"
+    }
+
+    expression = this.sumSubOperation(expression);
+
+    return expression;
+  }
+
   result() {
+    this.parentesisExpression(this.input);
     this.output = this.sumSubOperation(this.input);
   }
 
   clear() {
     this.input = "";
+  }
+
+  containsInvalidCharacters(input: string): boolean {
+    let allowedCharacters = "+-*/0123456789()[]{}.";
+    for (let char of input) {
+      if (allowedCharacters.indexOf(char) === -1) {
+        return true; // Caractere inválido encontrado
+      }
+    }
+    return false; // Nenhum caractere inválido encontrado
   }
 }
